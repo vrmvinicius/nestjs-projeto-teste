4ae174bb18488e4ac41648d4fc4b438d070449ca
Vinicius Migliorança <vinicius.miglioranca@g.globo>
2025-04-11 00:56:16 -0300

    Adiciona a configuração de extensões do gerenciador de entidades no bootstrap

--- src/main.ts
+++ src/main.ts
@@ -3,9 +3,12 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { NotFoundFilter } from './common/filters/not-found.filter'; // Importa o novo filtro
 import { ValidationFilter } from './common/filters/validation.filter';
+import { setupEntityManagerExtensions } from './infrastructure/database/extensions/entity-manager.extensions';
 
 async function bootstrap() {
    const app = await NestFactory.create(AppModule);
+
+   setupEntityManagerExtensions();
 
    app.useGlobalPipes(
       new ValidationPipe({
