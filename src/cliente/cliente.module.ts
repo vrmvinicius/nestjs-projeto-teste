6eadbe4eee564a0973bf4d6d511833790017cf4b
Vinicius Migliorança <vinicius.miglioranca@g.globo>
2025-04-11 00:56:36 -0300

    Adiciona o handler de atualização de cliente ao módulo de cliente

--- src/cliente/cliente.module.ts
+++ src/cliente/cliente.module.ts
@@ -1,6 +1,7 @@
 import { Logger, Module } from '@nestjs/common';
 import { CqrsModule } from '@nestjs/cqrs';
 import { ClientesController } from './cliente.controller';
+import { AtualizarClienteHandler } from './commands/atualizar-cliente/atualizar-cliente.handler';
 import { CriarClienteHandler } from './commands/criar-cliente/criar-cliente.handler';
 import { ObterClienteTodosHandler } from './queries/obter-cliente-todos/obter-cliente-todos.handler';
 import { ObterClienteHandler } from './queries/obter-cliente/obter-cliente.handler';
@@ -8,6 +9,6 @@ import { ObterClienteHandler } from './queries/obter-cliente/obter-cliente.handl
 @Module({
    imports: [CqrsModule],
    controllers: [ClientesController],
-   providers: [ObterClienteHandler, ObterClienteTodosHandler, CriarClienteHandler, Logger],
+   providers: [ObterClienteHandler, ObterClienteTodosHandler, CriarClienteHandler, AtualizarClienteHandler, Logger],
 })
 export class ClienteModule {}
