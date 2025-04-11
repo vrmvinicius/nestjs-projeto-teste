7bdfcefef3363f2121dbd803cc4f169415792673
Vinicius Migliorança <vinicius.miglioranca@g.globo>
2025-04-11 00:57:18 -0300

    Refatora a classe Cliente para utilizar propriedades privadas e métodos de acesso consistentes

--- src/cliente/entities/cliente.entity.ts
+++ src/cliente/entities/cliente.entity.ts
@@ -1,63 +1,66 @@
+import { EntityBase } from '@/common/domain/entities/entity.base';
 import { Email } from '../value-objects/email.value-object';
 import { Telefone } from '../value-objects/telefone.value-object';
 
-export class Cliente {
-   private id: number;
-   private nome: string;
-   private email: Email;
-   private telefone: Telefone;
-   private ativo: boolean;
-   private dataCadastro: Date;
+export class Cliente extends EntityBase {
+   private _id: number;
+   private _nome: string;
+   private _email: Email;
+   private _telefone: Telefone;
+   private _ativo: boolean;
+   private _dataCadastro: Date;
 
-   get Id(): number {
-      return this.id;
+   get id(): number {
+      return this._id;
    }
-   get Nome(): string {
-      return this.nome;
+   get nome(): string {
+      return this._nome;
    }
-   get Email(): Email {
-      return this.email;
+   get email(): Email {
+      return this._email;
    }
-   get Telefone(): Telefone {
-      return this.telefone;
+   get telefone(): Telefone {
+      return this._telefone;
    }
-   get Ativo(): boolean {
-      return this.ativo;
+   get ativo(): boolean {
+      return this._ativo;
    }
-   get DataCadastro(): Date {
-      return this.dataCadastro;
+   get dataCadastro(): Date {
+      return this._dataCadastro;
    }
 
    // Este setter é necessário para que o ORM possa atualizar o ID após a inserção no banco de dados.
-   private set Id(value: number) {
-      this.id = value;
+   set id(value: number) {
+      this._id = value;
    }
 
-   private constructor() {}
+   private constructor() {
+      super();
+   }
 
    public static criar(params: { nome: string; email: string; telefone: string }): Cliente {
       const cliente = new Cliente();
-      cliente.nome = params.nome;
-      cliente.email = new Email(params.email);
-      cliente.telefone = new Telefone(params.telefone);
-      cliente.ativo = true;
-      cliente.dataCadastro = new Date();
+      cliente._nome = params.nome;
+      cliente._email = new Email(params.email);
+      cliente._telefone = new Telefone(params.telefone);
+      cliente._ativo = true;
+      cliente._dataCadastro = new Date();
       return cliente;
    }
 
    public desativar(): void {
-      this.ativo = false;
+      this._ativo = false;
    }
 
    public atualizarNome(novoNome: string): void {
-      this.nome = novoNome;
+      this._nome = novoNome;
    }
 
    public atualizarEmail(novoEmail: string): void {
-      this.email = new Email(novoEmail);
+      this._email = new Email(novoEmail);
    }
 
    public atualizarTelefone(novoTelefone: string): void {
-      this.telefone = new Telefone(novoTelefone);
+      this._telefone = new Telefone(novoTelefone);
    }
 }
