6ac601b5ea460aa1c21b4f60fbaf4f591da1a431
Vinicius Migliorança <vinicius.miglioranca@g.globo>
2025-04-11 17:54:16 -0300

    adiciona o mapeamento de pedidos à configuração do MikroORM

--- src/mikro-orm.config.ts
+++ src/mikro-orm.config.ts
@@ -3,9 +3,10 @@ import { Migrator } from '@mikro-orm/migrations';
 import { TsMorphMetadataProvider } from '@mikro-orm/reflection';
 import { SqliteDriver } from '@mikro-orm/sqlite';
 import { ClienteMapping } from './infrastructure/mappings/cliente.mapping';
+import { PedidoMapping } from './infrastructure/mappings/pedido.mapping';
 
 const config: Options = {
-   entities: [ClienteMapping],
+   entities: [ClienteMapping, PedidoMapping],
    entitiesTs: ['src/**/*.entity.ts'],
    driver: SqliteDriver, // Changed from 'type' to 'driver'
    dbName: 'database/projeto-teste.sqlite',
