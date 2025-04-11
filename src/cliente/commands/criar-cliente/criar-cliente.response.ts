475242728486a788ef48ab3301792ad1fd3e36ec
Vinicius Migliorança <vinicius.miglioranca@g.globo>
2025-04-11 17:54:44 -0300

    adiciona a propriedade 'pedidos' à resposta de criação de cliente

--- src/cliente/commands/criar-cliente/criar-cliente.response.ts
+++ src/cliente/commands/criar-cliente/criar-cliente.response.ts
@@ -1,15 +1,26 @@
+import { Pedido } from '@/cliente/entities/pedido.entity';
+
 export class CriarClienteResponse {
    public readonly id: number;
    public readonly nome: string;
    public readonly email: string;
    public readonly telefone: string;
    public readonly dataCadastro: Date;
+   public readonly pedidos: Pedido[] = [];
 
-   constructor(params: { id: number; nome: string; email: string; telefone: string; dataCadastro: Date }) {
+   constructor(params: {
+      id: number;
+      nome: string;
+      email: string;
+      telefone: string;
+      dataCadastro: Date;
+      pedidos: Pedido[];
+   }) {
       this.id = params.id;
       this.nome = params.nome;
       this.email = params.email;
       this.telefone = params.telefone;
       this.dataCadastro = params.dataCadastro;
+      this.pedidos = params.pedidos;
    }
 }
